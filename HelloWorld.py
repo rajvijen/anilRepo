5e80aeacfb2139b8eb03cca7dac022001f20973c
Vijendra Kumar <vijendra.cute@gmail.com>
2021-09-27 16:49:45 +0000

    add odd even program

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,17 @@
-print("Hello World!!!")
+def oddEven(n):
+    if n % 2 == 0:
+        return 1
+    else:
+        return 0
+
+
+n = int(input("Enter a number: "))
+
+for i in range(2, n):
+    # print("Number is: ", oddEven(i))
+    x = oddEven(i)
+    if (x):
+        x = x + 1
+    else:
+        x = x - 1
+    print("x is: ", x)
